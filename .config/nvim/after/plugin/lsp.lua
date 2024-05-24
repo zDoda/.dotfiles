7d486f91bae0d752fab00a3af71fcde46d3679ac
Camdyn Zook <camdynzook@gmail.com>
2024-05-24 00:00:01 -0400

    Automated commit wiht updated .dotfiles

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -70,7 +70,7 @@ require('mason-lspconfig').setup()
 local servers = {
   clangd = {},
   -- gopls = {},
-  pylsp = {},
+  pyright = {},
   rust_analyzer = {},
   -- tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
