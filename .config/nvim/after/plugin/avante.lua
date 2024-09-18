07dabee05ba0335b86292b8e90c90ec274ff40fd
Camdyn Zook <camdynzook@gmail.com>
2024-09-18 00:00:01 -0400

    Automated commit wiht updated .dotfiles

--- .config/nvim/after/plugin/avante.lua
+++ .config/nvim/after/plugin/avante.lua
@@ -1,13 +1,91 @@
-require('avante').setup({
-    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  provider = "claude", -- Recommend using Claude
-  auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-3-5-sonnet-20240620",
+---NOTE: user will be merged with defaults and
+---we add a default var_accessor for this table to config values.
+
+require("avante").setup({
+  debug = false,
+  ---@alias Provider  "openai" -- | "azure" | "gemini" | "cohere" | "copilot" | "claude" | [string]
+  provider = "openai", -- Only recommend using Claude
+  auto_suggestions_provider = "openai",
+  ---@alias Tokenizer "tiktoken" | "hf"
+  -- Used for counting tokens and encoding text.
+  -- By default, we will use tiktoken.
+  -- For most providers that we support we will determine this automatically.
+  -- If you wish to use a given implementation, then you can override it here.
+  tokenizer = "tiktoken",
+  ---@alias AvanteSystemPrompt string
+  -- Default system prompt. Users can override this with their own prompt
+  -- You can use `require('avante.config').override({system_prompt = "MY_SYSTEM_PROMPT"}) conditionally
+  -- in your own autocmds to do it per directory, or that fit your needs.
+  system_prompt = [[
+You are an excellent programming expert.
+]],
+  ---@type AvanteSupportedProvider
+  openai = {
+    endpoint = "https://api.openai.com/v1",
+    model = "gpt-4o",
+    timeout = 30000, -- Timeout in milliseconds
+    temperature = 0,
+    max_tokens = 4096,
+    ["local"] = false,
+  },
+  ---@type AvanteSupportedProvider
+  copilot = {
+    endpoint = "https://api.githubcopilot.com",
+    model = "gpt-4o-2024-05-13",
+    proxy = nil, -- [protocol://]host[:port] Use this proxy
+    allow_insecure = false, -- Allow insecure server connections
+    timeout = 30000, -- Timeout in milliseconds
     temperature = 0,
     max_tokens = 4096,
   },
+  ---@type AvanteAzureProvider
+  azure = {
+    endpoint = "", -- example: "https://<your-resource-name>.openai.azure.com"
+    deployment = "", -- Azure deployment name (e.g., "gpt-4o", "my-gpt-4o-deployment")
+    api_version = "2024-06-01",
+    timeout = 30000, -- Timeout in milliseconds
+    temperature = 0,
+    max_tokens = 4096,
+    ["local"] = false,
+  },
+  ---@type AvanteSupportedProvider
+  claude = {
+    endpoint = "https://api.anthropic.com",
+    model = "claude-3-5-sonnet-20240620",
+    timeout = 30000, -- Timeout in milliseconds
+    temperature = 0,
+    max_tokens = 8000,
+    ["local"] = false,
+  },
+  ---@type AvanteSupportedProvider
+  gemini = {
+    endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+    model = "gemini-1.5-flash-latest",
+    timeout = 30000, -- Timeout in milliseconds
+    temperature = 0,
+    max_tokens = 4096,
+    ["local"] = false,
+  },
+  ---@type AvanteSupportedProvider
+  cohere = {
+    endpoint = "https://api.cohere.com/v1",
+    model = "command-r-plus-08-2024",
+    timeout = 30000, -- Timeout in milliseconds
+    temperature = 0,
+    max_tokens = 4096,
+    ["local"] = false,
+  },
+  ---To add support for custom provider, follow the format below
+  ---See https://github.com/yetone/avante.nvim/README.md#custom-providers for more details
+  ---@type {[string]: AvanteProvider}
+  vendors = {},
+  ---Specify the behaviour of avante.nvim
+  ---1. auto_apply_diff_after_generation: Whether to automatically apply diff after LLM response.
+  ---                                     This would simulate similar behaviour to cursor. Default to false.
+  ---2. auto_set_keymaps                : Whether to automatically set the keymap for the current line. Default to true.
+  ---                                     Note that avante will safely set these keymap. See https://github.com/yetone/avante.nvim/wiki#keymaps-and-api-i-guess for more details.
+  ---3. auto_set_highlight_group        : Whether to automatically set the highlight group for the current line. Default to true.
+  ---4. support_paste_from_clipboard    : Whether to support pasting image from clipboard. This will be determined automatically based whether img-clip is available or not.
   behaviour = {
     auto_suggestions = false, -- Experimental stage
     auto_set_highlight_group = true,
@@ -15,8 +93,22 @@ require('avante').setup({
     auto_apply_diff_after_generation = false,
     support_paste_from_clipboard = false,
   },
+  history = {
+    storage_path = vim.fn.stdpath("state") .. "/avante",
+    paste = {
+      extension = "png",
+      filename = "pasted-%Y-%m-%d-%H-%M-%S",
+    },
+  },
+  highlights = {
+    ---@type AvanteConflictHighlights
+    diff = {
+      current = "DiffText",
+      incoming = "DiffAdd",
+    },
+  },
   mappings = {
-    --- @class AvanteConflictMappings
+    ---@class AvanteConflictMappings
     diff = {
       ours = "co",
       theirs = "ct",
@@ -40,34 +132,45 @@ require('avante').setup({
       normal = "<CR>",
       insert = "<C-s>",
     },
+    -- NOTE: The following will be safely set by avante.nvim
+    ask = "<leader>aa",
+    edit = "<leader>ae",
+    refresh = "<leader>ar",
+    toggle = {
+      default = "<leader>at",
+      debug = "<leader>ad",
+      hint = "<leader>ah",
+      suggestion = "<leader>as",
+    },
     sidebar = {
       switch_windows = "<Tab>",
       reverse_switch_windows = "<S-Tab>",
     },
   },
-  hints = { enabled = true },
   windows = {
-    ---@type "right" | "left" | "top" | "bottom"
-    position = "right", -- the position of the sidebar
+    ---@alias AvantePosition "right" | "left" | "top" | "bottom"
+    position = "right",
     wrap = true, -- similar to vim.o.wrap
-    width = 30, -- default % based on available width
+    width = 30, -- default % based on available width in vertical layout
+    height = 30, -- default % based on available height in horizontal layout
     sidebar_header = {
       align = "center", -- left, center, right for title
       rounded = true,
     },
-  },
-  highlights = {
-    ---@type AvanteConflictHighlights
-    diff = {
-      current = "DiffText",
-      incoming = "DiffAdd",
+    input = {
+      prefix = "> ",
+    },
+    edit = {
+      border = "rounded",
     },
   },
-  --- @class AvanteConflictUserConfig
+  --- @class AvanteConflictConfig
   diff = {
     autojump = true,
-    ---@type string | fun(): any
-    list_opener = "copen",
+  },
+  --- @class AvanteHintsConfig
+  hints = {
+    enabled = true,
   },
 })
 
